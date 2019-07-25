b34518d1d970e966deb72abb2bec66b56e9130ba
matkuh <matkuh@gmail.com>
2019-07-25 14:50:27 -0700

    Updates

---
+++ precache-manifest.a49b57fc5be855406a341278475b33d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f208dea695bbe6af464a1ca3f0336ae",
+    "url": "/clicker-game/index.html"
+  },
+  {
+    "revision": "b417aa9a08913203a033",
+    "url": "/clicker-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicker-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b417aa9a08913203a033",
+    "url": "/clicker-game/static/js/main.254059c0.chunk.js"
+  },
+  {
+    "revision": "cd401eb4e9e99ab75dd2",
+    "url": "/clicker-game/static/js/runtime~main.8efb6c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicker-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
