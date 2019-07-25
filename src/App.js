9437ddcaa89e4fb594e942f8f7c656364e743ba8
matkuh <matkuh@gmail.com>
2019-07-25 16:12:04 -0700

    add nav, hero, card, footer component, start app.js, import components and render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Wrapper from './components/Wrapper'
+import Card from './components/Card'
+import Hero from './components/Hero'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        hello
-    </div>
-  );
-}
 
+class App extends Component {
+  state = {
+    celebs
+  }
+
+  shuffleCards = id => {
+    const celebs = this.state.celebs.map(celeb => celeb.id !== id);
+    this.setState({ celebs })
+  }
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav />
+        <Hero>Memory Clicker</Hero>
+        <Card>
+
+        </Card>
+      </Wrapper>
+    )
+  }
+
+
+}
 export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img src={props.image}/>
+            </div>
+        </div>
+    )
+}
+
+export default Card
